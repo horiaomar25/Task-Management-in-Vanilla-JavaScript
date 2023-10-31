55c633a1bd3a3aeaa6d142e3e60fd71f42c22788
horiaomar25 <horiaomar6@gmail.com>
2023-10-31 18:58:13 +0000

    dashboard updated with top 3 tasks in each grid

---
+++ client/db.js
@@ -0,0 +1,52 @@
+// Top 3 tasks to be added to the database.
+function createDBCard(task) {
+  const taskElementDB = document.createElement("div");
+  taskElementDB.className = "task-title";
+  taskElementDB.innerHTML = `
+    <p>${task.task_name}</>
+    
+    
+    `;
+
+  return taskElementDB;
+}
+
+function appendToDashBoard(task) {
+  // Get the task type
+  const taskDB = task.task_type;
+  // Create task card in weekly/daily grid
+  const taskGrid = createDBCard(task);
+
+  if (taskDB === "Daily") {
+    const dailyTask = document.querySelector(".daily-container");
+    dailyTask.appendChild(taskGrid);
+  } else if (taskDB === "Weekly") {
+    const weeklyTask = document.querySelector(".weekly-container");
+    weeklyTask.appendChild(taskGrid);
+  }
+}
+
+
+
+// This fetch request takes the data send to the database.
+// forEach loops through the data object and appends each task(array) to a task card with
+// the function appendTaskToDOM up above.
+const taskData = fetch("http://localhost:3001/tasks/")
+  .then((response) => {
+    if (response.ok) {
+      return response.json(); // Parse the JSON response
+    } else {
+      throw new Error("Failed to fetch data from the server");
+    }
+  })
+  .then((data) => {
+    data.data.forEach((task) => {
+      appendToDashBoard(task);
+      console.log(task)
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+  
--- client/tasks.js
+++ client/tasks.js
@@ -1,7 +1,7 @@
 const addButton = document.querySelector("#create_task"); // Use '#' to select by ID
 const taskForm = document.getElementById("pop-up");
 const closeButton = document.getElementById("close-button");
-const submitButton = document.getElementById('add-button');
+const submitButton = document.getElementById("add-button");
 
 // Click on create task and open the form.
 addButton.addEventListener("click", function () {
@@ -13,122 +13,122 @@ closeButton.addEventListener("click", function () {
   taskForm.style.display = "none";
 });
 
-submitButton.addEventListener('click', function () {
+// Closes the form once the Add Task has been clicked. It will send the form data to the server.
+submitButton.addEventListener("click", function () {
   taskForm.style.display = "none";
-})
+});
 
-//fetch data/ send data
-const formToServer = document.getElementById('add-button').addEventListener('click', function(e){
-  e.preventDefault();
-  const formData= new FormData(document.getElementById('taskForm'));
-  const task_name = formData.get('task_name');
-const task_description = formData.get('task_description');
-const task_type = formData.get('task_type');
+// This allows the form information to be sent to the server.
+const formToServer = document
+  .getElementById("add-button")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    const formData = new FormData(document.getElementById("taskForm"));
+    const task_name = formData.get("task_name");
+    const task_description = formData.get("task_description");
+    const task_type = formData.get("task_type");
 
-  // sendDataToServer({ task_name, task_description, task_type})
-  const data = {
-    task_name,
-    task_description,
-    task_type,
-    completed: false,
-  };
+    // Create a object using the value/data from the different input fields in the form. 
+    const data = {
+      task_name,
+      task_description,
+      task_type,
+      completed: false,
+    };
+ // Calls the function written below and give it the data object containing the inputs sent to the form.
+    sendDataToServer(data);
 
-  sendDataToServer(data)
-  
-  function sendDataToServer (data){
-  fetch('http://localhost:3001/tasks/',{
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  
-  })
-  .then(response => {
-    if (response.ok) {
-      // Data sent successfully, handle the response if needed
-      return response.json();
-    } else {
-      // Handle errors
-      throw new Error('Failed to send data to the server');
+    function sendDataToServer(data) {
+      fetch("http://localhost:3001/tasks/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => {
+          if (response.ok) {
+            // Data sent successfully, handle the response if needed
+            return response.json();
+          } else {
+            // Handle errors
+            throw new Error("Failed to send data to the server");
+          }
+        })
+        .then((data) => {
+          // Handle the response from the server, e.g., show a success message or update the UI
+          console.log(data.message); // Data saved successfully
+        })
+        .catch((error) => {
+          console.error(error);
+          // Handle and display the error
+        });
     }
-  })
-  .then(data => {
-    // Handle the response from the server, e.g., show a success message or update the UI
-    console.log(data.message); // Data saved successfully
-    
-  })
-  .catch(error => {
-    console.error(error);
-    // Handle and display the error
   });
-}
-
-})
 
 // Fetch Data from the database/server
 function createTaskCard(task) {
-  const taskElement= document.createElement('div');
-  taskElement.className ="card";
+  const taskElement = document.createElement("div");
+  taskElement.className = "card";
   taskElement.innerHTML = `
-  <h5>${task.task_name}</h5>
+  <h4>${task.task_name}</h4>
   <p>${task.task_description}</p>
   
   `;
-  
-   return taskElement;
+
+
+  return taskElement;
 }
 
+// Adding the tasks to the Task page in the right columns. 'task' is the data Object holding
 function appendTaskToDOM(task) {
   // get the task type
-   const taskType = task.task_type;
-   // Create new task card element
+  const taskType = task.task_type;
+  // Create new task card element
   const taskElement = createTaskCard(task);
 
-if(taskType === 'Daily'){
-  const dailyTask = document.querySelector('.Daily-task');
+  if (taskType === "Daily") {
+    const dailyTask = document.querySelector(".Daily-task");
     dailyTask.appendChild(taskElement);
-   } else if (taskType === 'Weekly') {
-    const weeklyTask = document.querySelector('.Weekly-task');
+  } else if (taskType === "Weekly") {
+    const weeklyTask = document.querySelector(".Weekly-task");
     weeklyTask.appendChild(taskElement);
-   }
-
- 
+  }
 }
 
-const taskData = fetch('http://localhost:3001/tasks/')  
-.then((response) => {
-  if (response.ok) {
+
+
+// This fetch request takes the data send to the database.
+// forEach loops through the data object and appends each task(array) to a task card with
+// the function appendTaskToDOM up above. 
+const taskData = fetch("http://localhost:3001/tasks/")
+  .then((response) => {
+    if (response.ok) {
       return response.json(); // Parse the JSON response
-  } else {
-      throw new Error('Failed to fetch data from the server');
-  }
-  
-})
-.then((data) => {
-  data.data.forEach((task) => {
-    appendTaskToDOM(task);
+    } else {
+      throw new Error("Failed to fetch data from the server");
+    }
   })
-  
-})
-.catch((error) => {
-  console.error(error);
-});
+  .then((data) => {
+    data.data.forEach((task) => {
+      appendTaskToDOM(task);
+      console.log(task)
+    });
+  })
+  .catch((error) =>{
+    console.error(error);
+  });
+
+
+
 
 // close the task information pop up.
-const closeTaskInformation = document.querySelector('.close-task');
-const taskInformation = document.querySelector('.task-expand');
+const closeTaskInformation = document.querySelector(".close-task");
+const taskInformation = document.querySelector(".task-expand");
 
-closeTaskInformation.addEventListener('click', function (){
-  taskInformation.style.display='none';
-})
-
-// Dashboard - List the top 3 task items in containers.
-
-
-
-
-
-
+// close the task information pop up.
+closeTaskInformation.addEventListener("click", function () {
+  taskInformation.style.display = "none";
+});
 
 
